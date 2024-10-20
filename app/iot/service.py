68980cd886d6c2dfe03b3f4eb4288cc9c9d55495
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-20 21:58:22 +0300

    Solution

--- app/iot/service.py
+++ app/iot/service.py
@@ -1,3 +1,4 @@
+import asyncio
 import random
 import string
 from typing import Protocol
@@ -9,16 +10,20 @@ def generate_id(length: int = 8) -> str:
     return "".join(random.choices(string.ascii_uppercase, k=length))
 
 
-# Protocol is very similar to ABC, but uses duck typing
-# so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
+# Protocol is very similar to ABC,
+# but uses duck typing
+# so devices should not inherit for it
+# (if it walks like a duck, and quacks
+# like a duck, it's a duck)
 class Device(Protocol):
-    def connect(self) -> None:
-        ...  # Ellipsis - similar to "pass", but sometimes has different meaning
+    async def connect(self) -> None:
+        ...  # Ellipsis - similar to "pass",
+        # but sometimes has different meaning
 
-    def disconnect(self) -> None:
+    async def disconnect(self) -> None:
         ...
 
-    def send_message(self, message_type: MessageType, data: str) -> None:
+    async def send_message(self, message_type: MessageType, data: str) -> None:
         ...
 
 
@@ -26,8 +31,8 @@ class IOTService:
     def __init__(self) -> None:
         self.devices: dict[str, Device] = {}
 
-    def register_device(self, device: Device) -> str:
-        device.connect()
+    async def register_device(self, device: Device) -> str:
+        await device.connect()
         device_id = generate_id()
         self.devices[device_id] = device
         return device_id
@@ -39,11 +44,21 @@ class IOTService:
     def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
 
-    def run_program(self, program: list[Message]) -> None:
+    async def run_program(self, program: list[Message]) -> None:
         print("=====RUNNING PROGRAM======")
-        for msg in program:
-            self.send_msg(msg)
+        messages = [
+            asyncio.create_task(self.send_msg(msg))
+            for msg in program
+        ]
+
+        gather_messages = asyncio.gather(*messages)
+
+        await gather_messages
+
         print("=====END OF PROGRAM======")
 
-    def send_msg(self, msg: Message) -> None:
-        self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
+    async def send_msg(self, msg: Message) -> None:
+        task_message = asyncio.create_task(
+            self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
+        )
+        await task_message
